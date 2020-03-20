c044010642b4f5bdb15bc9d91eb41f3e341e6e90
Logan Van Hook <loganvan@att.net>
2020-03-20 12:50:04 -0400

    MVP+

--- index.js
+++ index.js
@@ -10,4 +10,75 @@ server.get("/", (req, res)=>
     res.status(200).send("ok");
 });
 
+server.get("/projects/", (req, res)=>
+{
+    DB.GetProjects().then((response)=>
+    {
+        res.status(200).json(response);
+    }).catch((error)=>{ res.status(500).send("error"); });
+});
+
+server.post("/projects/", (req, res)=>
+{
+    DB.AddProject(req.body).then((response)=>
+    {
+        res.status(200).json(response);
+    }).catch((error)=>{ res.status(500).send("error"); });
+});
+
+server.get("/projects/:id", (req, res)=>
+{
+    DB.GetProject(req.params.id).then((response)=>
+    {
+        let ProjectData =
+        {
+            ...response,
+            resources:[],
+            tasks:[]
+        };
+
+        DB.GetResources(ProjectData.id).then((response)=>
+        {
+            ProjectData.resources = response;
+            DB.GetTasks(ProjectData.id).then((response)=>
+            {
+                ProjectData.tasks = response;
+                res.status(200).json(ProjectData);
+            }).catch((error)=> { res.status(500).send("error"); });;
+        }).catch((error)=> { res.status(500).send("error"); });
+    }).catch((error)=>{ res.status(500).send("error"); });
+});
+
+server.get("/resources", (req, res)=>
+{
+    DB.GetResources().then((response)=>
+    {
+        res.status(200).json(response);
+    }).catch((error)=>{ res.status(500).send("error"); });
+});
+
+server.post("/resources", (req, res)=>
+{
+    DB.AddResource(req.body).then((response)=>
+    {
+        res.status(200).json(response);
+    }).catch((error)=>{ res.status(500).send("error"); });
+});
+
+server.get("/tasks", (req, res)=>
+{
+    DB.GetAllTasks().then((response)=>
+    {
+        res.status(200).json(response);
+    }).catch((error)=>{ res.status(500).send("error"); });
+});
+
+server.post("/tasks", (req, res)=>
+{
+    DB.AddTask(req.body).then((response)=>
+    {
+        res.status(200).json(response);
+    }).catch((error)=>{ res.status(500).send("error"); });
+});
+
 server.listen(5000);
